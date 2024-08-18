59c71a7e8c6e2f97b6a4b188dc5f9706ab94ee12
Mohd Shuaib <shoebsiddiqui4947@gmail.com>
2024-08-18 16:55:17 +0530

    SF

--- Routes/productRoute.js
+++ Routes/productRoute.js
@@ -4,13 +4,12 @@ const {
   getProductDetails,
   createProduct,
 } = require("../Controller/productController");
-const { isAuthenticatedUser } = require("../Middleware/auth");
 // const { isAuthenticatedUser, authorizedRoles } = require("../Middleware/auth");
 const router = express.Router();
 
 router.route("/create").get(createProduct);
 
-router.route("/products").get(isAuthenticatedUser, getAllProducts);
+router.route("/products").get(getAllProducts);
 
 router.route("/product/:id").get(getProductDetails);
 
